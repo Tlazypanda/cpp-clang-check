6609a1db2ff2924c2aa9eafce129c9269d545ad0
Sneha Mishra <33183263+Tlazypanda@users.noreply.github.com>
2020-07-17 16:10:32 +0530

    Update checkall.sh

--- checkall.sh
+++ checkall.sh
@@ -30,7 +30,7 @@ clang-tidy *.c *.h *.cpp *.hpp *.C *.cc *.CPP *.c++ *.cp *.cxx -checks=boost-*,b
 
 clang-format --style=llvm -i *.c *.h *.cpp *.hpp *.C *.cc *.CPP *.c++ *.cp *.cxx > clang-format-report.txt
 
-cppcheck --enable=all --std=c++11 --language=c++ --output-file=cppcheck-report.txt *
+cppcheck -iclang-format-report.txt -iclang-tidy-report.txt --enable=all --std=c++11 --language=c++ --output-file=cppcheck-report.txt *
 
 flawfinder --columns --context --singleline . > flawfinder-report.txt
 
